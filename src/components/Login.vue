49a082ff55a674f038f978f572fe01817d84e865
shiwen <1085674149@qq.com>
2024-07-29 09:36:32 +0800

    这个是初始化后的登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login-container">
+    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required/>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required/>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleLogin() {
+      console.log("logging in with", this.username, this.password)
+    }
+  }
+};
+</script>
+<style scoped>
+.login-container {
+  width: 300px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+</style>
